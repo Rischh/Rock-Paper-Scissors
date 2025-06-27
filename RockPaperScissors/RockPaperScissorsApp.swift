82f21c5bb07e8d0a09669585ef0ff12ed9739be7
Rischh <rischebourg.samuel@gmail.com>
2025-06-27 13:29:38 +0200

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Samuel Rischebourg on 27/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
